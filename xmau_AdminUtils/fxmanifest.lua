82c87b79778e3e2bcd80f581ab0c6f77c025ead3
Xmau <44125791+Xmaught@users.noreply.github.com>
2020-05-04 23:06:22 +0200

    Add files via upload

---
+++ xmau_AdminUtils/fxmanifest.lua
@@ -0,0 +1,17 @@
+game 'rdr3'
+fx_version 'adamant'
+rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
+
+--ui_page 'index.html'
+
+client_scripts {
+  'AdminUtilsClient.net.dll'
+}
+
+server_scripts {
+  'AdminUtilsServer.net.dll'
+}
+
+files {
+  'MenuAPI.dll'
+}
